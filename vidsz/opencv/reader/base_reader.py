7800a3cac1bc574e40cb1b85ff09f682342fa991
shivamswarnkar <ss8464@nyu.edu>
2021-08-09 22:35:51 +0530

    add batch support

--- vidsz/opencv/reader/base_reader.py
+++ vidsz/opencv/reader/base_reader.py
@@ -1,6 +1,6 @@
 """Implements vidsz.interfaces.IReader interface for OpenCV Backend.
 """
-from typing import Union, List
+from typing import Optional, Union, List
 
 import cv2
 import numpy as np
@@ -11,17 +11,33 @@ from vidsz.interfaces import IReader
 class Reader(IReader):
     """Video Reading wrapper around Opencv-Backend
     """
-    def __init__(self, source_name: Union[str, int], **kwargs) -> None:
+    def __init__(self,
+                 source_name: Union[str, int],
+                 batch_size: Optional[int] = None,
+                 dynamic_batch: Optional[bool] = False,
+                 **kwargs) -> None:
         """Initiate Reader object
 
         Args:
             source_name (Union[str, int]): Name/URL/Path to video source
+
+            batch_size (Optional[int]): number of frames to return (as one batch) for one read.
+            Defaults to None will return images individually without batch axis.
+
+            dynamic_batch (Optional[bool]): if set to True then last batch of frames may have
+            less than batch_size frames (depending on how many frames were left for last batch).
+            If set to False, last batch may have some frames made up of zeros to match batch_size.
+            Defaults to False.
         """
         # initiate props
         self._initiate_props()
 
         # sanity conversion
         self._name = str(source_name)
+
+        # set batch
+        self._batch_size = batch_size
+        self._dynamic_batch = dynamic_batch
 
         # open video stream
         self._video_stream = cv2.VideoCapture(
@@ -44,6 +60,8 @@ class Reader(IReader):
         self._frame_count = 0
         self._seconds = 0
         self._minutes = 0
+        self._batch_size = None
+        self._dynamic_batch = False
 
     def _update_info(self) -> None:
         """Update info property according to currently open video stream
@@ -160,7 +178,7 @@ class Reader(IReader):
         """
         return self._video_stream.isOpened() and self._is_open
 
-    def read(self) -> Union[np.ndarray, None]:
+    def read_frame(self) -> Union[np.ndarray, None]:
         """Returns next frame from the video if available
 
         Returns:
@@ -171,8 +189,47 @@ class Reader(IReader):
         self._is_open = flag
         return frame
 
+    def read_batch(self) -> Union[np.ndarray, None]:
+        """Returns next batch of frames from the video if available
+
+        Returns:
+            Union[np.ndarry, None]: next batch if available, None otherwise.
+        """
+        if not self.is_open():
+            return None
+
+        # pre-allocate batch
+        batch = np.zeros((self._batch_size, self.height, self.width, 3))
+
+        # fill batch
+        for i in range(self._batch_size):
+            # read frame
+            frame = self.read_frame()
+
+            # stop process, no frames left
+            if frame is None:
+                # decrm index because this frame was empty
+                i -= 1
+                break
+
+            # add to batch
+            batch[i] = frame
+
+        return batch[:i + 1] if self._dynamic_batch else batch
+
+    def read(self) -> Union[np.ndarray, None]:
+        """Returns next frame or batch of frames from the video if available
+
+        Returns:
+            Union[np.ndarry, None]: next frame or batch of frames if available, None otherwise.
+        """
+        if self._batch_size is None:
+            return self.read_frame()
+        return self.read_batch()
+
     def read_all(self) -> List[np.ndarray]:
-        """Read all the frames into a list
+        """Read all the frames into a list.
+        Returns list of batches of frames if batch_size was set.
 
         Returns:
             List[np.ndarray]: List containing all the remaining frames in Video

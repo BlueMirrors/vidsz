6c4cd8163476b44b46143b489ed970f22f98d978
shivamswarnkar <shivamswarnkar18@gmail.com>
2022-02-02 21:37:47 +0000

    add batch benchmark

--- test/opencv/benchmark_allocation_strategy.py
+++ test/opencv/benchmark_allocation_strategy.py
@@ -2,6 +2,7 @@
 This info is used in batch reading logic."""
 import time
 import argparse
+import sys
 import numpy as np
 
 
@@ -22,7 +23,7 @@ def dynamic_allocate(batch_size, height=720, width=1280):
     return np.array(batch)
 
 
-def benchmark(iterations, batch_size):
+def benchmark_allocate(iterations, batch_size):
     start = time.time()
     for _ in range(iterations):
         pre_allocate(batch_size)
@@ -36,10 +37,37 @@ def benchmark(iterations, batch_size):
     print("Time Taken (Dynamic-Allocate):", round(time.time() - start, 3))
 
 
+def benchmark_reader_batch(iterations, batch_size):
+    sys.path.insert(0, './')
+    from vidsz.opencv import Reader
+    fpath = "static/countdown.mp4"
+
+    # batch
+    start = time.time()
+    for _ in range(iterations):
+        with Reader(fpath, batch_size=batch_size) as reader:
+            for frames in reader:
+                continue
+    
+    print(f"Time taken (batch): ", round(time.time() - start, 3))
+
+    # without-batch
+    start = time.time()
+    for _ in range(iterations):
+        with Reader(fpath) as reader:
+            for frame in reader:
+                continue
+    
+    print(f"Time taken (non-batch): ", round(time.time() - start, 3))
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-itrs", type=int, default=1000)
     parser.add_argument("-batch-size", type=int, default=64)
+    parser.add_argument("-test-allocate", action="store_true")
 
     opt = parser.parse_args()
-    benchmark(opt.itrs, opt.batch_size)
+    if opt.test_allocate:
+        benchmark_allocate(opt.itrs, opt.batch_size)
+    else:
+        benchmark_reader_batch(opt.itrs, opt.batch_size)
